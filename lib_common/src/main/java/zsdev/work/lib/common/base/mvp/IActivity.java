4581b301c4b9de6882111cf86ca8da771058e9ba
ZSAndroid <2393579405@qq.com>
2023-10-07 22:53:54 +0800

    delete：移除测试module，保留aar库，不再维护，此库仅作为遗留记录。

--- lib_common/src/main/java/zsdev/work/lib/common/base/mvp/IActivity.java
+++
@@ -1,9 +0,0 @@
-package zsdev.work.lib.common.base.mvp;
-
-/**
- * Created: by 2023-09-20 11:03
- * Description: Activity通用UI业务接口
- * Author: 张松
- */
-public interface IActivity extends IView {
-}
--- lib_common/src/main/java/zsdev/work/lib/common/base/mvp/IFragment.java
+++
@@ -1,9 +0,0 @@
-package zsdev.work.lib.common.base.mvp;
-
-/**
- * Created: by 2023-09-20 11:02
- * Description: Fragment通用UI业务接口
- * Author: 张松
- */
-public interface IFragment extends IView {
-}
